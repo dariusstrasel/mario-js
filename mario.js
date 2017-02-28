a360f61ea43aeb366e7a267d20a46d3c313de454
Dstrasel <siruku@gmail.com>
2017-02-27 20:23:45 -0500

    Refactored printPyramid to be right-aligned.

--- mario.js
+++ mario.js
@@ -12,10 +12,26 @@
  *       #####
  *      ######
  */
-function printPyramid(height) {
-    if (height == undefined) {
-        height = Number(prompt("Pick a height:\n"))
+function createLine(counter, maxLength) {
+    whiteSpace = maxLength - counter;
+    line = "";
+    while (line.length <= whiteSpace){
+        line += " "
     }
-    for (var brick = "##"; brick.length <= height + 1; brick+="#")
-        console.log(brick)
+    for (var lineLength = 1; lineLength <= counter; lineLength++)
+        line += "#";
+    return line
 }
+
+function printPyramid(height) {
+    for (var step = 2; step <= height + 1; step++)
+        console.log(createLine(step, height))
+}
+
+// function printPyramid(height) {
+//     if (height == undefined) {
+//         height = Number(prompt("Pick a height:\n"))
+//     }
+//     for (var brick = "##"; brick.length <= height + 1; brick+="#")
+//         console.log(brick)
+// }
